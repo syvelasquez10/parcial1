757ce6b6751e8c41adbf915542dd083a4ed3f025
Sergio Velásquez <sy.velasquez10@uniandes.edu.co>
2017-03-09 23:15:49 -0500

    Ultima versión

--- src/busqueda.js
+++ src/busqueda.js
@@ -7,29 +7,56 @@ class Busqueda extends Component {
     super(props);
     this.state = {
       busqueda: '',
-      colores:['','Blue','Purple','Orange','Yellow','Green']
+      colores:['','Blue','Purple','Orange','Yellow','Green'],
+      valores:[ true, true, true, true, true, true]
     }
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleInputChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    var valor = !(this.state.valores[event.target.name]);
+    this.state.valores.splice(event.target.name, 1, valor);
+    this.forceUpdate();
   }
 
   render() {
       return(
         <div>
           <div className="row">
-            <div className="col-md-5">
-              <input size="60" type="text" name="busqueda" value={this.state.busqueda} onChange={this.handleInputChange} placeholder="Escribe algo para buscar" required />
+            <div className="col-md-4">
+              <input size="35" type="text" name="busqueda" value={this.state.busqueda} onChange={this.handleInputChange} placeholder="Write something to look for it"/>
+            </div>
+            <div className="col-md-8">
+                  {this.state.colores.map((color, index) =>{
+                    if(this.state.busqueda) {
+                      if(index===this.state.colores.length-1) {
+                        return <h4>Click one of the buttons to toogle the column of images of that color</h4>
+                      }
+                      if(color===''){
+                        color="Sincolor";
+                      }
+                      return <button key={index} name={index} onClick={this.handleClick} className="btn btn-default rainbowImage" style={{color: color}}>{color}</button>
+                    }
+                  })}
             </div>
           </div>
           <br/>
+          <h1 className="noMargin">Your rainbow</h1>
           <div className="row">
             {this.state.colores.map((color, index) =>{
-              return <Columna key={index} busqueda={this.state.busqueda} color={color} url={this.props.url}/>
+              console.log(this.state.valores[index]);
+              if(this.state.valores[index]){
+                console.log("as");
+                return <Columna key={index} index={index} busqueda={this.state.busqueda} color={color} url={this.props.url} colores={this.state.colores}/>
+              }
             })}
           </div>
         </div>
--- src/columna.js
+++ src/columna.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import FlipMove from 'react-flip-move';
-import Imagen from './imagen';
-
 
 class Columna extends Component {
 
@@ -31,14 +28,24 @@ class Columna extends Component {
       return(
         <div className="col-md-2">
           {this.state.imagenes.map((imagen, index) =>{
-            return ( <FlipMove key={index} delay={1000} duration={600}>
-             <Imagen key={index} imagen={imagen} index={index} />
-           </FlipMove>);
+            var img="http://farm5.staticflickr.com/"+imagen.server+"/"+imagen.id+"_"+imagen.secret+".jpg";
+            return (
+              <img alt="normal" className="rainbowImage" width="100px" height="100px" key={index} src={img}/>
+            );
           })}
         </div>
       );
     } else {
-      return(<div></div>);
+      if(this.props.index===this.props.colores.length-1){
+        return (
+          <div>
+            <br/>
+            <p>Write Something in the text input to get a rainbow of flickr images</p>
+          </div>
+        );
+      } else {
+        return <div></div>
+      }
     }
   }
 }
